cfcc9fbffeb32e7fd25e2135303a95cfc52a9edf
Mo Lin <molin@wanchain.org>
2020-04-06 11:59:58 +0800

    finish

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const ApiInstance = require('iwan-sdk');
+const BN = require('bn.js')
+const keythereum = require("keythereum");
+const wanUtil = require('wanchain-util');
+const sleep = require('ko-sleep');
+const fs = require('fs');
+var Tx = wanUtil.wanchainTx;
 
 //In order to get an YourApiKey and YourSecretKey, sign up at iWan. Then create a new project to get a new YourApiKey and YourSecretKey key pair.
 let YourApiKey = "ae8a283e999e4fbba7ea54d6692806efea33b51bf797128a32d3d352d7a214e2";
 let YourSecretKey = "7897dde450229bb1da66c8cda21639d95b0248949ad48c1b618f99f2b7e23c31";
 
 //Subject to https://iwan.wanchain.org
+
+// Mainnet
+// let option = {
+//   url: "api2.wanchain.org",
+//   port: 8443,
+//   flag: "ws",
+//   version: "v3"
+// };
+
+// Testnet
 let option = {
-  url: "api2.wanchain.org",
+  url: "apitest.wanchain.org",
   port: 8443,
   flag: "ws",
   version: "v3"
@@ -14,56 +30,71 @@ let option = {
 
 let apiTest = new ApiInstance(YourApiKey, YourSecretKey, option);
 
-let testArray = [
-  ['getBalance', 'WAN', '0x4e6b5f1abdd517739889334df047113bd736c546'],
-  ['getRegTokens', 'ETH'],
-  ['getMultiTokenInfo', 'ETH', ["0xc5bc855056d99ef4bda0a4ae937065315e2ae11a","0x7017500899433272b4088afe34c04d742d0ce7df"]],
-  ['getEpochID', 'WAN'],
-  ['getSlotID', 'WAN'],
-  ['getStakerInfo', 'WAN', 5989868],
-  ['getEpochIncentivePayDetail', 'WAN', 18253],
-  ['getEpochIncentivePayDetail', 'WAN', 18203],
-  ['getValidatorInfo', 'WAN', '0x7212b9e259792879d85ca3227384f1005437e5f5'],
-  ['getValidatorStakeInfo', 'WAN', '0x7212b9e259792879d85ca3227384f1005437e5f5'],
-  ['getValidatorTotalIncentive', 'WAN', '0x7212b9e259792879d85ca3227384f1005437e5f5'],
-  ['getDelegatorStakeInfo', 'WAN', '0x4e6b5f1abdd517739889334df047113bd736c546'],
-  ['getDelegatorIncentive', 'WAN', '0x4e6b5f1abdd517739889334df047113bd736c546'],
-  ['getDelegatorTotalIncentive', 'WAN', '0x4e6b5f1abdd517739889334df047113bd736c546'],
-  ['getCurrentEpochInfo', 'WAN'],
-  ['getCurrentStakerInfo', 'WAN'],
-  ['getSlotCount', 'WAN'],
-  ['getSlotTime', 'WAN'],
-  ['getValidatorSupStakeInfo', 'WAN', ['0x7212b9e259792879d85ca3227384f1005437e5f5']],
-  ['getDelegatorSupStakeInfo', 'WAN', '0x4e6b5f1abdd517739889334df047113bd736c546'],
-  ['getEpochIncentiveBlockNumber', 'WAN', 18253],
-  ['getEpochStakeOut', 'WAN', 18253],
-  ['getMultiBalances', 'WAN', ['0x7212b9e259792879d85ca3227384f1005437e5f5', '0x4e6b5f1abdd517739889334df047113bd736c546']],
-
-];
+let keyPassword = "wanglu";
+let keystoreStr = fs.readFileSync("/Users/molin/Desktop/bf2.keystore.json", "utf8");
+let keystore = JSON.parse(keystoreStr);
+let keyAObj = { version: keystore.version, crypto: keystore.crypto };
+var privKeyA = keythereum.recover(keyPassword, keyAObj);
+let privateKey = privKeyA;
+let myAddr = '0x' + keystore.address;
+let targetGas = 220000000000;
+let gGasLimit = '0x' + (200000).toString(16);
+let gGasPrice = '0x' + (targetGas).toString(16);
+var gValue = '0x0';
 
 async function main() {
-  try {
-    console.log('Total:', testArray.length);
-    let result = await apiTest.getBalance('WAN', '0x4e6b5f1abdd517739889334df047113bd736c546');
-    console.log('balance:', result);
+  for (; true;) {
+    try {
+      let result = await apiTest.getGasPrice('WAN');
+      console.log('gasPrice:', result, (new Date()).toString());
 
-    for (let i=0; i<testArray.length; i++) {
-      await apiCall(testArray[i][0], testArray[i][1], testArray[i][2]);
+      if (result < targetGas) {
+        let nonce = await apiTest.getNonce('WAN', myAddr)
+        console.log("nonce:", nonce);
+        let ret = await transfer_one(myAddr, nonce);
+        console.log('send result:', ret);
+      }
+
+      await sleep(5000);
+    } catch (err) {
+      console.log(err);
+      apiTest.close();
+      process.exit(1);
     }
-    apiTest.close();
-    process.exit(0);
-  } catch (err) {
-    console.log(err);
-    apiTest.close();
-    process.exit(1);
   }
+  // apiTest.close();
+  // process.exit(0);
 }
 
-let count = 0;
-async function apiCall(name, chain = 'WAN', value) {
-  let start = Date.now();
-  await apiTest[name](chain, value);
-  console.log(count++, name, (Date.now() - start)/1000 + ' s');
+async function transfer_one(toAddr, serial) {
+  var rawTx = {
+    Txtype: '0x01',
+    nonce: serial,
+    gasPrice: gGasPrice,
+    gasLimit: gGasLimit,
+    to: toAddr,
+    value: gValue
+  };
+
+  const tx = new Tx(rawTx);
+  tx.sign(privateKey);
+  const serializedTx = tx.serialize();
+  console.log('serializedTx', "0x" + serializedTx.toString('hex'));
+  let txHash = await apiTest.sendRawTransaction('WAN', "0x" + serializedTx.toString('hex'));
+  console.log('txHash', txHash);
+  if (txHash) {
+    for(let i=0;i<10;i++) {
+      try {
+        let receipt = await apiTest.getTransactionReceipt('WAN', txHash);
+        console.log('txReceipt status:', receipt.status);
+        return receipt.status;
+      } catch (error) {
+        console.log(error);
+        await sleep(5000);
+      }
+    }
+  }
+  return false;
 }
 
 main();
